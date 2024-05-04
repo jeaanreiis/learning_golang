d92ddfbfaa8203c4078d83fa298880a323c68178
jeanreis-me <jeanreis@duck.com>
2024-05-03 21:14:34 -0300

    estudando constante

---
+++ cp3_variaveis/constantes.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const x int = 10
+	const y string = "\nHello, world!" // const nao inicia com variavel separada da declaracao
+
+	fmt.Println(x, y)
+}
